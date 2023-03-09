7289b50c143be73fcc12552f5149a10c72e4aa24
lucasmoraist <luksmnt1101@gmail.com>
2023-03-09 09:42:36 -0300

    Atributos das classes criado. Agora as classes estão sendo extendidos pela classe macronutrientes

--- src/main/java/br/com/fiap/healthydelight/models/Alimento.java
+++ src/main/java/br/com/fiap/healthydelight/models/Alimento.java
@@ -1,13 +1,10 @@
 package br.com.fiap.healthydelight.models;
 
-public class Alimento {
+public class Alimento extends Macronutrientes{
     private long id_alimento;
     private String alimento;
     private float kcal;
     private int porcao;
-    private float proteina;
-    private float gordura;
-    private float carboidrato;
 
     public long getId_alimento() {
         return id_alimento;
@@ -33,38 +30,15 @@ public class Alimento {
     public void setPorcao(int porcao) {
         this.porcao = porcao;
     }
-    public float getProteina() {
-        return proteina;
-    }
-    public void setProteina(float proteina) {
-        this.proteina = proteina;
-    }
-    public float getGordura() {
-        return gordura;
-    }
-    public void setGordura(float gordura) {
-        this.gordura = gordura;
-    }
-    public float getCarboidrato() {
-        return carboidrato;
-    }
-    public void setCarboidrato(float carboidrato) {
-        this.carboidrato = carboidrato;
-    }
-
+    
     public Alimento() {}
 
-    public Alimento(long id_alimento, String alimento, float kcal, int porcao, float proteina, float gordura,
-            float carboidrato) {
+    public Alimento(float proteina, float gordura, float carboidrato, long id_alimento, String alimento, float kcal,
+            int porcao) {
+        super(proteina, gordura, carboidrato);
         this.id_alimento = id_alimento;
         this.alimento = alimento;
         this.kcal = kcal;
         this.porcao = porcao;
-        this.proteina = proteina;
-        this.gordura = gordura;
-        this.carboidrato = carboidrato;
     }
-
-    
-    
 }
